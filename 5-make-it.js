a7793b1b8f6b77555065cd36c4821ab7f78c66fb
Arif Rizki <arifwise67@gmail.com>
2024-01-29 21:38:11 +0800

    day#5/366

---
+++ 5-make-it.js
@@ -0,0 +1,20 @@
+// WPU Coding challenge 2024
+// 5/366
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   if (distanceToPump <= mpg * fuelLeft) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   return distanceToPump <= mpg * fuelLeft ? true : false;
+// };
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  distanceToPump <= mpg * fuelLeft ? true : false;
+
+console.log(zeroFuel(100, 50, 1));
